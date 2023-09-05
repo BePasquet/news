3c72a6be8fc07a1f90890f44179f5ed3541ed631
Pedro Benjamin Lescano Pasquet <plescanopasquet@gmail.com>
2023-09-05 10:23:51 +0700

    - material ui set up

--- src/app/app-providers.tsx
+++ src/app/app-providers.tsx
@@ -4,7 +4,8 @@ import { AppRoutes } from './app-routes';
 const GlobalStyles = createGlobalStyle`
 html, body {
     margin: 0;
-    padding: 0    
+    padding: 0;
+    font-family: 'Roboto', 'sans-serif';     
 }
 `;
 

01644709750fd4297e192e8d42d5d29fca0df10d
Pedro Benjamin Lescano Pasquet <plescanopasquet@gmail.com>
2023-09-07 15:16:28 +0700

    - docker file added - readme added

--- src/app/shared/components/results/results.tsx
+++ src/app/shared/components/results/results.tsx
@@ -8,6 +8,42 @@ export interface ResultsProps<T = unknown> extends Omit<BaseState, 'loaded'> {
   loaderComponent?: ReactNode;
 }
 
+/**
+ * Utility to show results based on state (loading, error and data)
+ * @param params -
+ * - data used by children
+ * - loading flag that signifies loading state
+ * - error message to display
+ * @example
+ * export function App() {
+ *   const [loading, setLoading] = useState(false);
+ *   const [error, setError] = useState('false');
+ *   const [data, setData] = useState([]);
+ *
+ *   const fetchData = async () => {
+ *     try {
+ *       setLoading(true);
+ *       const response = await fetch('/example.com');
+ *       const result = await response.json();
+ *       setData(result);
+ *     } catch (e) {
+ *       setError(e as string);
+ *     } finally {
+ *       setLoading(false);
+ *     }
+ *   };
+ *
+ *   useEffect(() => {
+ *     fetchData();
+ *   }, []);
+ *
+ *   return (
+ *     <Results loading={loading} error={error} data={data}>
+ *       {data && JSON.stringify(data)}
+ *     </Results>
+ *   );
+ * }
+ */
 export function Results({
   loading,
   error,
